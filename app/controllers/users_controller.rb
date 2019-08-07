82fdd961d8307d90f92cef128729f5827585c765
masaki.aoba <masakiwan0426@gmail.com>
2019-08-07 22:33:55 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
+
 
   def new
     @user = User.new
